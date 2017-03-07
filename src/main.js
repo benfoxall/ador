29eeffb3b9a78f286d272e4cd0ac42de8371a79b
Ben Foxall <benfoxall@gmail.com>
2017-03-07 17:25:18 -0500

    Use mqtt.js instead of mat :(

--- src/main.js
+++ src/main.js
@@ -4,6 +4,24 @@ const connection = new Connection({
   target: document.querySelector('body')
 })
 
-connection.on('connected', () => {
-  console.log("Connected")
+connection.on('connected', (client) => {
+
+  console.log("Connected", client)
+
+  document.body.addEventListener('click', (e) => {
+    e.preventDefault()
+    const v = ~~(Math.random()*255)
+    client.publish('/debug/press', v)
+  }, false)
+
+  client.subscribe('/debug/press')
+
+  client.on('message', (topic, buffer) => {
+    const payload = buffer.toString()
+    console.log("message:", topic, payload)
+
+    document.body.style.backgroundColor =
+      `hsl(${payload}, 50%, 50%)`
+  })
+
 })
