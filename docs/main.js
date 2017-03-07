c8c652ade2b688928a3c594effaf3a5aebaf2899
Ben Foxall <benfoxall@gmail.com>
2017-03-07 12:26:06 -0500

    Bring over styles from mockup

--- docs/main.js
+++ docs/main.js
@@ -1,1 +1,9 @@
 console.log('hello');
+
+var qs = document.querySelector.bind(document);
+
+// hook up the url to something that was submitted
+var url_field = qs('[name=url]');
+var search = location.search||'';
+var match = search.match(/[?&]url=(.*)[&#]?/);
+url_field.value = match ? unescape(match[1]) : '';
--- src/main.js
+++ src/main.js
@@ -1,1 +1,9 @@
 console.log('hello')
+
+const qs = document.querySelector.bind(document)
+
+// hook up the url to something that was submitted
+const url_field = qs('[name=url]')
+const search = location.search||''
+const match = search.match(/[?&]url=(.*)[&#]?/)
+url_field.value = match ? unescape(match[1]) : ''
