156972aeb6605c031511e9773dffbf3e873e6c60
Ben Foxall <benfoxall@gmail.com>
2017-03-07 17:47:01 -0500

    Throw everything away for now

--- src/main.js
+++ src/main.js
@@ -1,27 +1,52 @@
-import Connection from './Connection.html'
+// import Connection from './Connection.html'
+//
+// const connection = new Connection({
+//   target: document.querySelector('header')
+// })
+//
+// connection.on('connected', (client) => {
+//
+//   console.log("Connected", client)
+//
+//   document.body.addEventListener('click', (e) => {
+//     e.preventDefault()
+//     const v = ~~(Math.random()*255)
+//     client.publish('/debug/press', v)
+//   }, false)
+//
+//   client.subscribe('/debug/press')
+//
+//   client.on('message', (topic, buffer) => {
+//     const payload = buffer.toString()
+//     console.log("message:", topic, payload)
+//
+//     document.body.style.backgroundColor =
+//       `hsl(${payload}, 50%, 50%)`
+//   })
+//
+// })
 
-const connection = new Connection({
-  target: document.querySelector('body')
-})
 
-connection.on('connected', (client) => {
 
-  console.log("Connected", client)
+var sections = Array.from(
+  document.querySelectorAll('li[tabindex]')
+)
 
-  document.body.addEventListener('click', (e) => {
-    e.preventDefault()
-    const v = ~~(Math.random()*255)
-    client.publish('/debug/press', v)
-  }, false)
+function setFocus(target) {
 
-  client.subscribe('/debug/press')
-
-  client.on('message', (topic, buffer) => {
-    const payload = buffer.toString()
-    console.log("message:", topic, payload)
-
-    document.body.style.backgroundColor =
-      `hsl(${payload}, 50%, 50%)`
+  sections.forEach(function(section){
+    section.className = target === section ? 'active' : 'inactive'
   })
 
+}
+
+sections.forEach(function(section){
+  section.addEventListener('focus', () => {
+    section.className = 'active'
+    // setFocus(section)
+  }, false)
+
+  section.addEventListener('blur', () => {
+    section.className = ''
+  }, false)
 })
