93fe0713f262cf8dd42fd149f26d45661b4a136d
Ben Foxall <benfoxall@gmail.com>
2017-03-07 14:12:56 -0500

    Hacky (but working-ish) connect ui

--- docs/main.js
+++ docs/main.js
@@ -1,9 +1,48 @@
-console.log('hello');
-
 var qs = document.querySelector.bind(document);
+
+var stateBar = qs('#state');
 
 // hook up the url to something that was submitted
 var url_field = qs('[name=url]');
 var search = location.search||'';
 var match = search.match(/[?&]url=(.*)[&#]?/);
 url_field.value = match ? unescape(match[1]) : '';
+
+var submit = qs('[type=submit]');
+
+url_field.addEventListener('keyup', function () {
+  form.dataset.state = '';
+  stateBar.className = '';
+});
+
+var form = qs('form');
+form.addEventListener('submit', function (e) {
+  e.preventDefault();
+
+  // connect
+  url_field.disabled = true;
+  submit.value = 'Connecting';
+
+  connect()
+    .then(function () {
+      // display the new ui, hide this one
+    })
+    .catch(function () {
+      // display error
+      form.dataset.state = 'error';
+      url_field.disabled = false;
+      stateBar.className = 'error';
+      submit.value = 'Connect';
+    });
+
+  // update the url for reloads
+  history.pushState({},document.name,("?url=" + (escape(url_field.value))));
+}, false);
+
+
+
+var connect = function () {
+  return new Promise(function (accept, reject) {
+    setTimeout(reject, 1000);
+  })
+};
--- src/main.js
+++ src/main.js
@@ -1,9 +1,48 @@
-console.log('hello')
-
 const qs = document.querySelector.bind(document)
+
+const stateBar = qs('#state')
 
 // hook up the url to something that was submitted
 const url_field = qs('[name=url]')
 const search = location.search||''
 const match = search.match(/[?&]url=(.*)[&#]?/)
-url_field.value = match ? unescape(match[1]) : ''
+url_field.value = match ? unescape(match[1]) : '';
+
+const submit = qs('[type=submit]')
+
+url_field.addEventListener('keyup', () => {
+  form.dataset.state = ''
+  stateBar.className = ''
+})
+
+const form = qs('form')
+form.addEventListener('submit', (e) => {
+  e.preventDefault()
+
+  // connect
+  url_field.disabled = true
+  submit.value = 'Connecting'
+
+  connect()
+    .then(() => {
+      // display the new ui, hide this one
+    })
+    .catch(() => {
+      // display error
+      form.dataset.state = 'error'
+      url_field.disabled = false
+      stateBar.className = 'error'
+      submit.value = 'Connect'
+    })
+
+  // update the url for reloads
+  history.pushState({},document.name,`?url=${escape(url_field.value)}`)
+}, false)
+
+
+
+const connect = () => {
+  return new Promise((accept, reject) => {
+    setTimeout(reject, 1000)
+  })
+}
